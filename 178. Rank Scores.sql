3ad70cf290a7fbdba7961cb036253663a7b2e337
Chrislc <acl8923@gmail.com>
2018-11-03 22:16:27 -0400

    keep correct answer

--- 178. Rank Scores.sql
+++ 178. Rank Scores.sql
@@ -1,28 +1,4 @@
 /* Write your PL/SQL query statement below */
--- select ROUND(sc.Score,2) Score, dense_rank() over (order by Score desc) Rank from Scores sc
-
--- select ROUND(sc.Score, 2 ) Score, 
-
--- dense_rank() over (order by Score desc) Rank from Scores sc
-
-
--- select Score,
-
--- dense_rank() over(order by Score desc) Rank
-
--- from scores
-
--- select Score, dense_Rank() over(order by Score desc) as Rank from Scores;
-
--- select ROUND(sc.Score,2) Score, dense_rank() over (order by Score desc) Rank from Scores sc
-
--- select Round(score, 2) Score,
-
--- dense_rank() over(order by Score desc) Rank
-
--- from Scores
-
-
 
 select round(score, 2) Score,
 
