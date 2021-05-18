e8a2d6f526679df3eeca0cd5e7475abbc90608a7
Chrislc <acl8923@gmail.com>
2021-05-18 09:21:05 -0400

    Update 55. Jump Game.java

--- 55. Jump Game.java
+++ 55. Jump Game.java
@@ -1,5 +1,19 @@
 class Solution {
+    
     public boolean canJump(int[] nums) {
+        int max = 0;
+        
+        for(int i = 0; i < nums.length;i++) {
+            if (i > max) {
+                return false;
+            }
+            max = Math.max(max, nums[i] + i);
+         }
+        
+        return max >= nums.length -1;
+    }
+    
+    public boolean canJump2(int[] nums) {
         
         int n = nums.length;
         
